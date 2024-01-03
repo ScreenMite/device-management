370c3e04d98b1067f4a0e1f538bae062dbacc94f
Bradley <bradcarnage@gmail.com>
2024-01-02 18:25:31 -0800

    appearance tweaks
    changed boot time from 1 second delay to 5 seconds
    installed unclutter (hides cursor)
    uploaded wallpaper
    set wallpaper

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# This script will be downloaded and ran automatically
+
 hostname=$(hostname)
 if [[ "$hostname" != "screenmite" ]]; then
     echo "setting hostname to screenmite"
@@ -6,12 +9,34 @@ else
     echo "The hostname is already set to screenmite"
 fi
 
+if [ ! -L "/usr/share/images/desktop-base/default_stock" ]; then
+    mv /usr/share/images/desktop-base/default /usr/share/images/desktop-base/default_stock
+else
+    rm /usr/share/images/desktop-base/default    
+fi
+rm -rf /usr/share/images/wallpaper.png
+mv $PWD/wallpaper.png /usr/share/images/wallpaper.png
+ln -s /usr/share/images/wallpaper.png /usr/share/images/desktop-base/default
+
+
+
+
 echo "Applying grub settings.."
 rm -rf /boot/grub/theme
 mv $PWD/grub_theme /boot/grub/theme
 FILE='/etc/default/grub'
-sed -i -e 's/GRUB_TIMEOUT=[0-9]\+/GRUB_TIMEOUT=1/g' $FILE
+sed -i -e 's/GRUB_TIMEOUT=[0-9]\+/GRUB_TIMEOUT=5/g' $FILE
 LINE='GRUB_THEME=/boot/grub/theme/theme.txt'
 sed -i -e 's/GRUB_THEME=.*\+/GRUB_THEME=/boot/grub/theme/theme.txt/g' $FILE
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 update-grub
+
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor1/workspace0/last-image -s /path/to/your/wallpaper.jpg
+
+
+echo "Installing unclutter (hides mouse cursor)"
+sudo apt-get install unclutter -y
+# unclutter will need a reboot for it to take effect
+
+
+
